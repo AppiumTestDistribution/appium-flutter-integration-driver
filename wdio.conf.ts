8f967f000add643612099cc43777fe6267494e8a
Sudharsan Selvaraj <sudharsanselvaraj.c@gmail.com>
2024-06-14 10:26:14 +0530

    Revert sample tests

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -41,7 +41,7 @@ export const config: Options.Testrunner = {
   // The path of the spec files will be resolved relative from the directory of
   // of the config file unless it's absolute.
   //
-  specs: ['./test/specs/**/sample*.js'],
+  specs: ['./test/specs/**/*.js'],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
