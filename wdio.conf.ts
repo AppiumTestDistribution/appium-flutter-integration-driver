90417583b3824f5b82df71b43c9b3c3fbe7abfc1
saikrishna321 <saikrishna321@yahoo.com>
2024-07-01 12:38:42 +0530

    increase timeout

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -108,10 +108,10 @@ export const config: Options.Testrunner = {
   //
   // Default timeout in milliseconds for request
   // if browser driver or grid doesn't send response
-  connectionRetryTimeout: 240000,
+  connectionRetryTimeout: 340000,
   //
   // Default request retries count
-  connectionRetryCount: 0,
+  connectionRetryCount: 1,
   //
   // Test runner services
   // Services take over a specific job you don't want to take care of. They enhance
