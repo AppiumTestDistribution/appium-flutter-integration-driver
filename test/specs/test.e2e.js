aced6a9ac38045e8159112611cae60fa670a4408
Sai Krishna V <saikrishna321@yahoo.com>
2025-09-15 17:33:22 +0530

    Fix pipeline
    Co-authored-by: SrinivasanTarget <srinivasan.sekar1990@gmail.com>

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -127,7 +127,7 @@ describe('My Login application', () => {
       });
       expect(await childElement.getText()).toEqual('Child 2');
    });
-   it.only('Ancestor Test', async () => {
+   it('Ancestor Test', async () => {
       await performLogin();
       await openScreen('Nested Scroll');
       const parentElement = await browser.flutterByAncestor$({
