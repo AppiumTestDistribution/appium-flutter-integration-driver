1953fe07e340f06e4f6336ebc6b8f1cc4705cba3
Sai Krishna V <saikrishna321@yahoo.com>
2025-09-16 16:24:37 +0530

    Fix issue on appLaunch
    Co-authored-by: SrinivasanTarget <srinivasan.sekar1990@gmail.com>

--- src/driver.ts
+++ src/driver.ts
@@ -506,7 +506,11 @@ export class AppiumFlutterDriver extends BaseDriver<FlutterDriverConstraints> {
          this.flutterPort,
       );
       await this.proxy?.command('/session', 'POST', {
-         capabilities: this.proxydriver.originalCaps,
+         capabilities: Object.assign(
+            {},
+            this.proxydriver.originalCaps?.alwaysMatch,
+            this.proxydriver.originalCaps?.firstMatch[0],
+         ),
       });
       return activateAppResponse;
    }
